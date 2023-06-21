bcfd488feb6c1f50dc6339e3b7d1307e504870c5
samettal <saltuntas717@gmail.com>
2023-06-21 12:28:26 +0300

    [12.0][FIX] button_save fixed and added new stock.picking field: is_packaged

--- delivery_package_barcode/wizard/delivery_package_barcode_wiz.py
+++ delivery_package_barcode/wizard/delivery_package_barcode_wiz.py
@@ -4,71 +4,87 @@ from odoo import _, api, fields, models
 
 
 class DeliveryPackageBarcodeWiz(models.TransientModel):
-    _name = 'delivery.package.barcode.wiz'
-    _inherit = 'barcodes.barcode_events_mixin'
-    _description = 'Wizard to read barcode for delivery package'
+    _name = "delivery.package.barcode.wiz"
+    _inherit = "barcodes.barcode_events_mixin"
+    _description = "Wizard to read barcode for delivery package"
     # To prevent remove the record wizard until 2 days old
     _transient_max_hours = 48
 
-    name = fields.Char(string='Name')
-    message = fields.Char('Message', default='Scan a barcode', readonly=1)
-    message_type = fields.Selection([('success', 'Success'), ('error', 'Error'),
-                                     ('info', 'Info')], default='info')
+    name = fields.Char(string="Name")
+    message = fields.Char("Message", default="Scan a barcode", readonly=1)
+    message_type = fields.Selection(
+        [("success", "Success"), ("error", "Error"), ("info", "Info")], default="info"
+    )
     barcode = fields.Char()
     picking_id = fields.Many2one(
-        comodel_name='stock.picking',
+        comodel_name="stock.picking",
     )
-    picking_line_ids = fields.One2many('stock.move', string='Picking Lines',
-                                       related='picking_id.move_ids_without_package')
-    package_count = fields.Integer('Package Count', default=0)
-    package_weight = fields.Float('Package Weight', default=0.0)
+    picking_line_ids = fields.One2many(
+        "stock.move",
+        string="Picking Lines",
+        related="picking_id.move_ids_without_package",
+    )
+    package_count = fields.Integer("Package Count", default=0)
+    package_weight = fields.Float("Package Weight", default=0.0)
 
-    @api.onchange('picking_id')
+    @api.onchange("picking_id")
     def onchange_picking_id(self):
-        self.update({
-            'package_count': self.picking_id.carrier_package_count,
-            'package_weight': self.picking_id.picking_total_weight,
-        })
+        self.update(
+            {
+                "package_count": self.picking_id.carrier_package_count,
+                "package_weight": self.picking_id.picking_total_weight,
+            }
+        )
 
     @api.model
     def create(self, vals):
         res = super(DeliveryPackageBarcodeWiz, self).create(vals)
         if not res.name:
-            res.name = res.picking_id.name + _(' - PACK BARCODE')
+            res.name = res.picking_id.name + _(" - PACK BARCODE")
         return res
 
     @api.one
     def button_save(self):
-        vals = {'carrier_package_count': self.package_count,
-                'picking_total_weight': self.package_weight}
+        vals = {
+            "carrier_package_count": self.package_count,
+            "picking_total_weight": self.package_weight,
+            "is_packaged": True,
+        }
         self.picking_id.write(vals)
         return True
 
     def process_barcode(self, barcode):
-        pick_obj = self.env['stock.picking']
+        pick_obj = self.env["stock.picking"]
 
-        if barcode and self.env.user.has_group('stock.group_stock_user'):
+        if barcode and self.env.user.has_group("stock.group_stock_user"):
             domain = self._barcode_domain(barcode)
             picking = pick_obj.search(domain, limit=1)
             if picking:
-                self.update({
-                    'picking_id': picking.id,
-                    'message': _('Picking found:'),
-                    'message_type': 'success',
-                })
+                self.update(
+                    {
+                        "picking_id": picking.id,
+                        "message": _("Picking found:"),
+                        "message_type": "success",
+                    }
+                )
             else:
-                self.update({
-                    'message': _('Picking not found'),
-                    'message_type': 'error',
-                })
+                self.update(
+                    {
+                        "message": _("Picking not found"),
+                        "message_type": "error",
+                    }
+                )
         return
 
     def _barcode_domain(self, barcode):
-        return ['|', '|', ('name', '=', barcode),
-                ('shipping_number', '=', barcode),
-                ('carrier_tracking_ref', '=', barcode)]
+        return [
+            "|",
+            "|",
+            ("name", "=", barcode),
+            ("shipping_number", "=", barcode),
+            ("carrier_tracking_ref", "=", barcode),
+        ]
 
     def on_barcode_scanned(self, barcode):
         self.barcode = barcode
         self.process_barcode(self.barcode)
-
